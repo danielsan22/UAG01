bb8ee6b27a4122c5453cb970f57e003fa8c53360
Daniel Sanchez <Daniel@MacBook-Pro-de-Daniel-2.local>
2014-09-27 12:55:01 -0500

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by Daniel Sanchez on 27/09/14.
+//  Copyright (c) 2014 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
